e0b4f8228b31c27e12e7927831024ccbd2c36b89
Gowthami Thallam <gowthamijanakiram587@gmail.com>
2026-03-26 06:15:32 +0000

    async code

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,67 +1,83 @@
+//used to define module.
 sap.ui.define([
+    //importing the base controller class.
     "sap/ui/core/mvc/Controller"
+    //function recieves the imported class.
 ], function (Controller) {
+
     "use strict";
-
+    //creating your own controller name.
     return Controller.extend("basicproject.controller.View1", {
-
+        //function runs when the view loads.
         onInit: function () {
 
             // --Comparision Operators-- //
-            var result = "";
-            result += "== Op Results:\n";
-            result += "0 == '' : " + (0 == "") + "\n";
-            result += '"10" == 10 : ' + ("10" == 10) + "\n";
-            result += "null == undefined : " + (null == undefined) + "\n";
-            result += "true == 1 : " + (true == 1) + "\n\n";
+            //a variable to store o/p.
+            let result = "";
 
-            result += "=== Op Results:\n";
-            result += "0 === '' : " + (0 === "") + "\n";
-            result += '"10" === 10 : ' + ("10" === 10) + "\n";
-            result += "null === undefined : " + (null === undefined) + "\n";
-            result += "true === 1 : " + (true === 1) + "\n\n";
+            result += "== Operator Res:\n";
+            result += "0 == false : " + (0 == false) + "\n";
+            result += '"3" == 3 : ' + ("3" == 3) + "\n\n";
 
-            // Shows Initial Result
+            result += "=== Operator Res:\n";
+            result += "1 === true : " + (1 === true) + "\n";
+            result += '"7" === 7: ' + ("7" === 7) + "\n\n";
+            //Gets UI element with ID txtResult & Displays the result text on screen
+
             this.getView().byId("txtResult").setText(result);
 
             // --Promises-- //
+            //creating a promise.
             const myPromise = new Promise((resolve, reject) => {
 
-                let success = true; // changed value
-
+                let success = true;
+                //Delay execution (2 seconds)
                 setTimeout(() => {
                     if (success) {
-                        resolve("Data loaded successfully");
+                        resolve("Promise: Data loaded successfully\n");
                     } else {
-                        reject("Error while loading data");
+                        reject("Promise: Error while loading data\n");
                     }
                 }, 2000);
 
             });
-
-            myPromise
-                .then(resultPromise => {
-                    result += resultPromise;
-                    this.getView().byId("txtResult").setText(result);
+            //Runs when promise is successful
+            myPromise.then(resultPromise => {
+                   let currentText = this.getView().byId("txtResult").getText();
+                   currentText += resultPromise;
+                   this.getView().byId("txtResult").setText(currentText);
                 })
+                //Runs if promise fails
                 .catch(error => {
-                    result += error;
-                    this.getView().byId("txtResult").setText(result);
+                    let currentText = this.getview().byId("txtResult").getText();
+                    currentText += error;
+                    this.getView().byId("txtResult").setText(currentText);
                 });
+
+            // --Calls Async Function-- //
+            this.example();
         },
+        //async function//
+        // --Updated async Function-- //
+        example: async function () {
+            //Store view in variable
+            let oView = this.getView();
+            let resultText = oView.byId("txtResult").getText();
 
-        // --Button Function(Imp)-- //
-        onCompare: function () {
+            // Print Start in UI
+            //Add Async section
+            resultText += "\nAsync/Await:\nStart\n";
+            oView.byId("txtResult").setText(resultText);
 
-            let val1 = this.getView().byId("ip1").getValue();
-            let val2 = this.getView().byId("ip2").getValue();
+            //Wait until promise completes
+            let result = await new Promise((resolve) => {
+                setTimeout(() => resolve("Done!\n"), 2000);
+            });
+            resultText=oView.byId("txtResult").getText();
+            // Print Done + End in UI
+            resultText += result + "End\n";
+            oView.byId("txtResult").setText(resultText);
 
-            let result = "";
-
-            result += "== Result: " + (val1 == val2) + "\n";
-            result += "=== Result: " + (val1 === val2);
-
-            this.getView().byId("txtResult").setText(result);
         }
 
     });
